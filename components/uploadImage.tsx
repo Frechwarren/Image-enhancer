6b11522fe40d4c4629fd6bfe67ffcc85fafff739
Frech <frechwarren120415@gmail.com>
2025-03-13 06:33:39 +0800

    add a cancel button, function that will reset the file input

--- components/uploadImage.tsx
+++ components/uploadImage.tsx
@@ -1,18 +1,29 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const UploadImage = () => {
   const [imagePreview, setImagePreview] = useState<string | null>();
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const uploadPhoto = (file: File | null) => {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
         setImagePreview(reader.result as string);
+        reader.abort();
       };
       reader.readAsDataURL(file);
+    }
+  };
+
+  const cancelUpload = () => {
+    setImagePreview(null);
+
+    // Reset the file input field
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
     }
   };
 
@@ -20,25 +31,35 @@ const UploadImage = () => {
     <div className="flex flex-col justify-center items-center">
       <div className="flex justify-center items-center gap-2 border-2 h-55 w-full p-1 mb-1.5">
         <Button
-          className="border-2 border-dashed w-50 h-50 bg-transparent text-black"
+          className="border-2 border-dashed w-50 h-50 bg-transparent text-gray-400 hover:text-gray-600"
           variant="outline"
         >
+          {!imagePreview ? " Upload photo here" : ""}
           <input
             type="file"
             accept="image/*"
+            ref={fileInputRef}
             className="absolute opacity-0 w-50 h-50 cursor-pointer"
             onChange={(e) => uploadPhoto(e.target.files?.[0] || null)}
           />
           <img
             src={imagePreview || undefined}
             className="flex w-full h-full object-contain"
+            hidden={!imagePreview}
           />
         </Button>
 
         <div className="border-2 border-dashed w-50 h-50"></div>
       </div>
-      <div className="flex justify-center items-center">
-        <Button className=" bg-green-600">Enhance</Button>
+      <div className="flex justify-center items-center gap-2">
+        <Button
+          className=" bg-red-800"
+          hidden={!imagePreview}
+          onClick={cancelUpload}
+        >
+          Cancel
+        </Button>
+        <Button className=" bg-green-800">Enhance</Button>
       </div>
     </div>
   );
